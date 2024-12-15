9cfaa0e329a25e3a308f38eed0ae5c2ff7459499
fenris <fabian.hanrath@posteo.de>
2024-12-15 01:54:13 +0100

    feat: working conversion to uf2

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use std::{
 
 use anyhow::{anyhow, Result};
 
-const UF2_MAGIC_START0: u32 = 0x0A324655;
+const UF2_MAGIC_START0: u32 = 0x0A324655; // "UF2\n"
 const UF2_MAGIC_START1: u32 = 0x9E5D5157;
 const UF2_MAGIC_END: u32 = 0x0AB16F30;
 const ADDRESS_MASK: u32 = 0xff;
-const INVERTED_ADDRESS_STUFF: u32 = !ADDRESS_MASK;
+const INVERTED_ADDRESS_MASK: u32 = !ADDRESS_MASK;
 
 struct Block {
     pub address: u32,
@@ -42,6 +42,8 @@ impl Block {
         header.extend_from_slice(&number_of_blocks.to_le_bytes());
         if family_id.is_some() {
             header.extend_from_slice(&family_id.unwrap().to_le_bytes());
+        } else {
+            header.extend_from_slice(&(0x00 as u32).to_le_bytes());
         }
 
         // Add the block's data
@@ -88,6 +90,11 @@ fn hex_to_uf2(hex_lines: impl Iterator<Item = String>, family_id: Option<u32>) -
             i += 2;
         }
 
+        let byte_count = binary_line[0] as usize;
+        if binary_line.len() - 5 != byte_count {
+            return Err(anyhow!("Invalid HEX line: mismatched byte count"));
+        }
+
         if binary_line.len() < 4 {
             return Err(anyhow!(
                 "Line with less than 4 encoded hex chars + 1 byte leads to undefined behaviour!"
@@ -96,28 +103,32 @@ fn hex_to_uf2(hex_lines: impl Iterator<Item = String>, family_id: Option<u32>) -
 
         match binary_line[3] {
             0 => {
-                let address = upper + (((binary_line[1] as u32) << 8) | (binary_line[2] as u32));
+                let mut address =
+                    upper + (((binary_line[1] as u32) << 8) | (binary_line[2] as u32));
                 if app_start_address.is_none() {
                     app_start_address = Some(address);
                 }
-                for hex_char in binary_line.iter().skip(3) {
+                // skip first 4 and last item
+                for index in 4..binary_line.len() - 1 {
+                    let byte = &binary_line[index];
                     if current_block.is_none()
-                        || (current_block.as_ref().unwrap().address & INVERTED_ADDRESS_STUFF)
-                            != (address & INVERTED_ADDRESS_STUFF)
+                        || (current_block.as_ref().unwrap().address & INVERTED_ADDRESS_MASK)
+                            != (address & INVERTED_ADDRESS_MASK)
                     {
-                        if current_block.is_some() {
-                            blocks.push(current_block.take().unwrap());
+                        if let Some(block) = current_block.take() {
+                            blocks.push(block);
                         }
-                        current_block = Some(Block::new(address & INVERTED_ADDRESS_STUFF));
+                        current_block = Some(Block::new(address & INVERTED_ADDRESS_MASK));
                     }
                     let current_mut_block = current_block
                         .as_mut()
                         .expect("current_block should always be some here");
 
-                    current_mut_block.bytes[(address & ADDRESS_MASK) as usize] = *hex_char as u8;
+                    current_mut_block.bytes[(address & ADDRESS_MASK) as usize] = *byte;
+                    address += 1;
                 }
             }
-            1 => break,
+            1 => break, // End of file
             2 => {
                 upper = (((binary_line[4] as u32) << 8) | (binary_line[5] as u32)) << 4;
             }
@@ -130,9 +141,9 @@ fn hex_to_uf2(hex_lines: impl Iterator<Item = String>, family_id: Option<u32>) -
         }
     }
 
-    if current_block.is_some() {
+    if let Some(block) = current_block.take() {
         // Add last block
-        blocks.push(current_block.take().unwrap());
+        blocks.push(block);
     }
 
     let number_of_blocks = blocks.len() as u32;
@@ -167,4 +178,51 @@ fn hex_to_uf2_file(hex_file: &Path, output_path: &Path) -> Result<()> {
         .expect("Error writing to destination");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Read;
+
+    use super::*;
+
+    #[test]
+    fn compare_to_python() {
+        hex_to_uf2_file(
+            Path::new("./test/fenris-rmk-central.hex"),
+            Path::new("./test/fenris-rmk-central.uf2"),
+        )
+        .unwrap();
+
+        let rust_reader = BufReader::new(File::open("./test/fenris-rmk-central.uf2").unwrap());
+        let python_reader = BufReader::new(File::open("./test/fenris-rmk-central_py.uf2").unwrap());
+
+        let mut rust_bytes = rust_reader.bytes();
+        let mut python_bytes = python_reader.bytes();
+
+        let mut position: usize = 0;
+
+        loop {
+            match (rust_bytes.next(), python_bytes.next()) {
+                (None, None) => break,
+                (Some(_), None) => {
+                    assert!(false, "rust_bytes is longer than python_bytes");
+                    break;
+                }
+                (None, Some(_)) => {
+                    assert!(false, "python_bytes is longer than rust_bytes");
+                    break;
+                }
+                (Some(rust_byte), Some(python_byte)) => {
+                    assert_eq!(
+                        rust_byte.unwrap(),
+                        python_byte.unwrap(),
+                        "Difference at {position}, hex: {position:X}"
+                    );
+                }
+            }
+
+            position += 1;
+        }
+    }
 }

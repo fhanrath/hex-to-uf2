5362a17a93dfa833abfad14a4ba0b47a79abbdda
fenris <fenris@fenris.xyz>
2024-12-23 01:09:15 +0100

    fix: make command work with cargo commands (the command name itself is passed as first argument)

--- cargo_hex_to_uf2/src/main.rs
+++ cargo_hex_to_uf2/src/main.rs
@@ -1,20 +1,43 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use hex_to_uf2::{families::ChipFamily, hex_to_uf2_file};
 
 /// Converts hex files to uf2
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    hex_file: PathBuf,
-    output_path: PathBuf,
-    #[arg(short, long)]
-    family: Option<ChipFamily>,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Adds files to myapp
+    #[command(name = "hex-to-uf2")]
+    HexToUf2 {
+        /// Path to input hex file
+        #[arg(short, long)]
+        input_path: PathBuf,
+        /// Path to output uf2 file
+        #[arg(short, long)]
+        output_path: PathBuf,
+        /// family of chips
+        #[arg(short, long)]
+        family: Option<ChipFamily>,
+    },
 }
 
 fn main() {
     let args = Cli::parse();
 
-    hex_to_uf2_file(&args.hex_file, &args.output_path, args.family).unwrap();
+    println!("{:?}", args);
+
+    match args.command {
+        Commands::HexToUf2 {
+            input_path,
+            output_path,
+            family,
+        } => hex_to_uf2_file(&input_path, &output_path, family).unwrap(),
+    }
 }
